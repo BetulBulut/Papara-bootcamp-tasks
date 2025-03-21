2bf4bae9b0dd44a598d0a6b80483a70cfd565600
BetulBulut <btlb2002@gmail.com>
2025-03-21 11:56:28 +0300

    Product.cs updated

--- WebApi/WebApi/Models/Product.cs
+++ WebApi/WebApi/Models/Product.cs
@@ -4,7 +4,7 @@ namespace RestfulApiProject.Models
 {
     public class Product
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
 
         [Required]
         public string Name { get; set; }
